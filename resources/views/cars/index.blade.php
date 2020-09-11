2faaec797d9f169c0478e9c13566b5cb73bae448
Nicolò <nico.paganelli91@gmail.com>
2020-09-11 19:06:04 +0200

    destroy

--- resources/views/cars/index.blade.php
+++ resources/views/cars/index.blade.php
@@ -7,6 +7,13 @@
             <a href="{{ route('cars.show', $car) }}">info</a>
             -
             <a href="{{ route('cars.edit', $car) }}">modifica</a>
+
+            <form action="{{ route('cars.destroy', $car) }}" method="post">
+                @csrf
+                @method('DELETE')
+
+                <input type="submit" value="elimina">
+            </form>
         </li>
     @endforeach
 </ul>
